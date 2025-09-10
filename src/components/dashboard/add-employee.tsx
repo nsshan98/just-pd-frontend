975af7e0b4207cd030fac59a3ce43df73118e20c
nsshan98 <n.sakibnss@gmail.com>
2025-09-10 22:31:59 +0600

    feat: enhance AddEmployeeDialog with form validation and submission handling

--- src/components/dashboard/add-employee.tsx
+++ src/components/dashboard/add-employee.tsx
@@ -11,45 +11,201 @@ import {
 } from "@/components/atoms/dialog";
 import { Input } from "@/components/atoms/input";
 import { Label } from "@/components/atoms/label";
+import { useCreateEmployee } from "@/hooks/reactQuery/employeeQuery";
+import { employeeSchema, EmployeeSchemaType } from "@/zod/employee-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { isAxiosError } from "axios";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../atoms/form";
 
 export default function AddEmployeeDialog() {
+  const { employeeCreateMutation } = useCreateEmployee();
+  const employeeCreateForm = useForm<EmployeeSchemaType>({
+    defaultValues: {
+      image: "",
+      name: "",
+      email: "",
+      show_email: true,
+      phone: "",
+      show_phone: true,
+      designation: "",
+      department: "",
+      sorting_order: 0,
+      is_published: true,
+    },
+    resolver: zodResolver(employeeSchema),
+  });
+
+  const getFormData = (data: EmployeeSchemaType) => {
+    const formData = new FormData();
+    if (data.image) {
+      formData.append("image", data.image);
+    }
+    formData.append("name", data.name);
+    formData.append("email", data.email);
+    formData.append("show_email", (data.show_email ?? true).toString());
+    formData.append("phone", data.phone);
+    formData.append("show_phone", (data.show_phone ?? true).toString());
+    formData.append("designation", data.designation);
+    formData.append("department", data.department);
+    formData.append("sorting_order", (data.sorting_order ?? 0).toString());
+    formData.append("is_published", (data.is_published ?? true).toString());
+    return formData;
+  };
+
+  const onSubmit = (data: EmployeeSchemaType) => {
+    const formData = getFormData(data);
+
+    employeeCreateMutation.mutate(formData, {
+      onSuccess: () => {
+        toast("Employee Created Successfully");
+      },
+      onError: (error: Error) => {
+        if (isAxiosError(error)) {
+          console.log(error);
+        }
+      },
+    });
+    console.log(data);
+  };
+
+  console.log(employeeCreateForm.formState.errors);
+
   return (
     <Dialog>
-      <form>
-        <DialogTrigger asChild>
-          <Button variant="outline">Add Employee</Button>
-        </DialogTrigger>
-        <DialogContent
-          className="sm:max-w-[425px]"
-          onInteractOutside={(e) => {
-            e.preventDefault();
-          }}
-          onEscapeKeyDown={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <DialogHeader>
-            <DialogTitle>Edit profile</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you&apos;re
-              done.
-            </DialogDescription>
-          </DialogHeader>
-          <div className="grid gap-4">
-            <div className="grid gap-3">
-              <Label htmlFor="name-1">Name</Label>
-              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="username-1">Username</Label>
-              <Input id="username-1" name="username" defaultValue="@peduarte" />
-            </div>
+      <DialogTrigger asChild>
+        <Button variant="outline">Add Employee</Button>
+      </DialogTrigger>
+      <DialogContent
+        className="sm:max-w-[425px]"
+        onInteractOutside={(e) => {
+          e.preventDefault();
+        }}
+        onEscapeKeyDown={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <DialogHeader>
+          <DialogTitle>Add Employee Form</DialogTitle>
+          <DialogDescription>
+            Fill the form below to add a new employee to the directory.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4">
+          <div className="grid gap-3">
+            <Form {...employeeCreateForm}>
+              <form
+                onSubmit={employeeCreateForm.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <FormField
+                  control={employeeCreateForm.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-amber-50 border-amber-950"
+                          placeholder="John Doe"
+                          type="text"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={employeeCreateForm.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-amber-50 border-amber-950"
+                          placeholder="example@email.com"
+                          type="email"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={employeeCreateForm.control}
+                  name="phone"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Phone</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-amber-50 border-amber-950"
+                          placeholder="+88 01234 567890"
+                          type="text"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="flex flex-row gap-2">
+                  <FormField
+                  control={employeeCreateForm.control}
+                  name="designation"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Designation</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-amber-50 border-amber-950"
+                          placeholder="+88 01234 567890"
+                          type="text"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={employeeCreateForm.control}
+                  name="department"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Department</FormLabel>
+                      <FormControl>
+                        <Input
+                          className="bg-amber-50 border-amber-950"
+                          placeholder="+88 01234 567890"
+                          type="text"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                </div>
+                <DialogFooter>
+                  <Button type="submit">Submit</Button>
+                </DialogFooter>
+              </form>
+            </Form>
           </div>
-          <DialogFooter>
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </form>
+        </div>
+      </DialogContent>
     </Dialog>
   );
 }
